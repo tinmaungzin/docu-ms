74c5e5f7590b7b0bf55ee80eed2904212ef4aafb
zawlinnnaing <zawlinnnaing0018@gmail.com>
2019-11-04 01:30:27 +0630

    added guest middleware

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,6 +7,12 @@ use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('guest:stu',['except' => 'logout']);
+    }
+
     public function showLoginForm()
     {
         return view('Login.login');
@@ -14,21 +20,20 @@ class LoginController extends Controller
 
     public function login(Request $request)
     {
-        $credentials =$request->validate([
-            'school_mail'=> 'required|email|ends_with:mtu.edu.mm',
-            'password'=> 'required'
+        $credentials = $request->validate([
+            'school_mail' => 'required|email|ends_with:mtu.edu.mm',
+            'password'    => 'required'
         ]);
-        if(Auth::guard('stu')->attempt($credentials)){
+        if (Auth::guard('stu')->attempt($credentials)) {
             return redirect('documents');
-        }
-        elseif(Auth::guard('hod')->attempt($credentials)){
+        } elseif (Auth::guard('hod')->attempt($credentials)) {
 
             return redirect(route('hods.show'));
-        }
-        else{
-            return redirect('login')->withErrors(['password'=> 'Email or Password incorrect!']);
+        } else {
+            return redirect('login')->withErrors(['password' => 'Email or Password incorrect!']);
         }
     }
+
     public function logout()
     {
         Auth::guard('stu')->logout();
