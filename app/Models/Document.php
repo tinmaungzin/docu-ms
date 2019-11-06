0a885a58ff469ae297def698a5c6210d130ee883
zawlinnnaing <zawlinnnaing0018@gmail.com>
2019-11-06 14:40:32 +0630

    modified relationships

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -7,13 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * @property string title
  * @property string abstract
+ * @property string filename
  */
 class Document extends Model
 {
     protected $guarded = ['id'];
+
     public function keywords()
     {
-        return $this->hasMany(Keyword::class);
+        return $this->hasMany(Keyword::class, 'document_id');
     }
 
     public function student()
--- app/Models/Keyword.php
+++ app/Models/Keyword.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Keyword extends Model
 {
     //
+    protected $table = 'keywords';
+
+    protected $fillable = ['document_id', 'name'];
+
+    public function document()
+    {
+        return $this->belongsTo(Document::class, 'document_id');
+    }
+
 }
