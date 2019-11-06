dd3678170eeef5038701c13d12d05d6ba474b67b
zawlinnnaing <zawlinnnaing0018@gmail.com>
2019-11-06 14:39:06 +0630

    removed old keywords tables and create new keywords table

---
+++ database/migrations/2019_11_06_080033_create_new_keywords_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewKeywordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('keywords', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('document_id');
+            $table->string('name', 255);
+            $table->timestamps();
+            $table->foreign('document_id')->references('id')->on('documents')->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        Schema::dropIfExists('keywords');
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+    }
+}
