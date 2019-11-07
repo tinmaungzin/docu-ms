92f4714e0cd3304a2eba957d29a4af3ed5df5e60
zawlinnnaing <zawlinnnaing0018@gmail.com>
2019-11-08 01:32:29 +0630

    changed cron job from daily to every two hours

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('save:document')
-            ->daily()
+            ->cron('0 */2 * * *')
             ->timezone('Asia/Yangon');
     }
 
